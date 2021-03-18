bb914f85d4dd8fe3ae722536bf5f4f3dd994495d
dawson1096 <dawson1096@gmail.com>
2021-03-18 02:25:03 -0400

    bug fix

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 db.on("error", console.error.bind(console, "MongoDB connection error"));
